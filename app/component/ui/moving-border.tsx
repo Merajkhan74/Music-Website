04b2abf5b3f5fa5aee6c4d0b895642e65d5e12c0
meraj <merajkhan05km@gmail.com>
2026-03-06 02:03:26 +0530

    fix useRef error

--- app/component/ui/moving-border.tsx
+++ app/component/ui/moving-border.tsx
@@ -82,8 +82,9 @@ export const MovingBorder = ({
   ry?: string;
   [key: string]: any;
 }) => {
-  const pathRef = useRef<any>();
-  const progress = useMotionValue<number>(0);
+const pathRef = useRef<any>(null);
+
+const progress = useMotionValue<number>(0);
 
   useAnimationFrame((time) => {
     const length = pathRef.current?.getTotalLength();
